53a090223eeff16a9d386bb24c7d3055d140f705
Curtis Schofield & Jen-Mei Wu <pair+curtis+jen-mei@blazingcloud.net>
2011-04-26 14:22:38 -0700

    Added failing test for supporting forms with fields with the same name.

---
+++ lib/capybara/spec/session/multi_param_spec.rb
@@ -0,0 +1,22 @@
+shared_examples_for "multi_params" do
+
+  describe "allows many elements to share the same form name in array style syntax" do
+    before do
+      @session.visit('/form')
+    end
+    it "without clobbering" do
+      @session.check('form_soup_spicy')
+      @session.check('form_soup_mushrooms')
+      @session.check('form_soup_bamboo_shoots')
+      @session.choose('soup_gluten-free')
+      @session.fill_in('form_special_request', :with =>'Bow to the soup please')
+      @session.fill_in('form_order_number',    :with =>'94')
+      @session.select('Soba',       :from => 'form_soup_addon')
+
+      @session.click_button "awesome"
+      
+      soup=extract_results(@session)["soup"]
+      soup.should have(7).entries
+    end
+  end
+end
